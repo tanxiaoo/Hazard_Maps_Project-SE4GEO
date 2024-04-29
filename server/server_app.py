f814d90d601531b531eb9259d8479c68646e6bef
GioMontefoschi <giovannimontefoschi@gmail.com>
2024-04-29 12:28:37 +0200

    Server work

--- server/server_app.py
+++ server/server_app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine, text
+import pandas as pd
+import geopandas as gpd
 
 app = Flask(__name__)
 
+#DB connection
+engine = create_engine('postgresql://postgres:Acnaap1300@localhost:5433/se4geo')
+con = engine.connect()
+
+#Endpoints
+
 @app.route('/')
 def prova():
-    return "<p>Facciamo progressi</p>"
+    return render_template('index.html')
+
+@app.route('/status')
+def server_status():
+    return "<p>Status: operativo</p>"
+if __name__ == '__main__':
+    app.run()
+
